9185d4c602fa408fe3acd6f305700b3c4db56652
Dave Filion <dave.filion@gmail.com>
2017-04-26 15:14:32 -0400

    initial commit

---
+++ ddl/transactions.sql
@@ -0,0 +1,10 @@
+CREATE TABLE transactions (
+        date_of_sale DATE,
+        brand TEXT,
+        category TEXT,
+        season TEXT,
+        sku TEXT,
+        description TEXT,
+        cost MONEY,
+        price MONEY
+);
---
+++ loader.py
@@ -0,0 +1,68 @@
+import csv
+import pdb
+import psycopg2
+import re
+import argparse
+
+TABLE_NAME = "transactions"
+
+def connect_to_db():
+    conn = psycopg2.connect(dbname="df", user="df", host="localhost")
+    conn.autocommit = True
+    return conn.cursor()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="load data into database")
+    parser.add_argument("-f", "--file", type=str, help="file to upload")
+    return parser.parse_args()
+
+def main(args):
+    with open(args.file, 'rU') as csvfile:
+        pg = connect_to_db()
+
+        print "Truncating transactions table"
+        truncate_sql = """
+        TRUNCATE TABLE transactions;
+        """
+        pg.execute(truncate_sql)
+        
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        header = False
+        for row in reader:
+            if not header:
+                header = True
+                pass
+            else:
+                date_of_sale = row[0]
+                brand = row[2]
+                category = row[3]
+                season = row[4]
+                sku = row[5]
+                description = row[6]
+                cost = row[7]
+                price = row[8]
+
+                if date_of_sale != "":
+                    sql = """
+                    INSERT INTO {table} (date_of_sale, brand, category, season, sku, description, cost, price)
+                    VALUES ('{date}', '{brand}', '{category}', '{season}', '{sku}', '{description}', {cost}, {price})
+                    """.format(
+                            table=TABLE_NAME, 
+                            date=date_of_sale, 
+                            brand=brand, 
+                            category=category, 
+                            season=season, 
+                            sku=sku,
+                            description=description, 
+                            cost=cost, 
+                            price=price)
+                    try:
+                        pg.execute(sql)
+                    except Exception, e:
+                        print "Error on row {}".format(row)
+                        print e
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
